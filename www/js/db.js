bd14d6c7602b5cfb6769ca6e923c9b0edaf001b0
dedspr <douglas_emanoel@hotmail.com>
2017-11-14 18:06:24 -0200

    Ajustes

--- www/js/db.js
+++ www/js/db.js
@@ -318,7 +318,7 @@ $(function () {
 
         dataSelectValido: function (transaction, results) {
 
-            var strData = "13/11/2017";
+            var strData = "21/11/2017";
             var partesData = strData.split("/");
             var data = new Date(partesData[2], partesData[1] - 1, partesData[0]);
             if (data < new Date()) {
